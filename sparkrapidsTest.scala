3844f40230ce62379545fe726630e47a86b7c217
mydoors <zhengjieormydoors@gmail.com>
2023-12-21 18:45:18 +0800

    add

--- sparkrapidsTest.scala
+++ sparkrapidsTest.scala
@@ -16,7 +16,7 @@ val data = Seq("hello", "world", "this", "is", "rapids").toDF("value")
 processWithRapids(data)
 
 
-val df = spark.read.format("csv").option("header", "true").load("/root/spark/data/packets.csv")
+val df = spark.read.format("csv").option("header", "true").load("/root/spark/data/network_data.csv")
 df.printSchema()
 df.show()
 df.count()
