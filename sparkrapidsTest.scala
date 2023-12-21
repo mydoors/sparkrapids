c25b1e89d1cbf64ac6545d077aadb3eeef5b1666
mydoors <zhengjieormydoors@gmail.com>
2023-12-21 18:08:21 +0800

    add

--- sparkrapidsTest.scala
+++ sparkrapidsTest.scala
@@ -21,21 +21,22 @@ df.printSchema()
 df.show()
 df.count()
 // Map 操作：Filter
-// 过滤出 ip.src 或 ip.dst 等于 "192.168.5.16" 的记录
+// 过滤出 ip.src 或 ip.dst 等于 "192.168.5.160" 的记录
 // Map 操作：Filter
-// 过滤出 ip.src 或 ip.dst 等于 "192.168.5.16" 的记录
-val filteredDF = df.filter($"`ip.src`" === "192.168.5.16" || $"`ip.dst`" === "192.168.5.16")
+// 过滤出 ip.src 或 ip.dst 等于 "192.168.5.160" 的记录
+// Map 操作：Filter
+// 过滤出 ip.src 或 ip.dst 等于 "192.168.5.162" 的记录
+val filteredDF = df.filter($"`ip.src`" === "192.168.5.162" || $"`ip.dst`" === "192.168.5.162")
 
-// 为了在自连接中区分两个 DataFrame，给 'frame.number' 字段加上别名
+// 对同一个 DataFrame 执行自连接（self-join）以查找具有相同源 IP 和目标 IP 但协议不同的记录
 val df1Renamed = filteredDF.withColumnRenamed("frame.number", "frame_number1").withColumnRenamed("_ws.col.Protocol", "protocol1")
 val df2Renamed = filteredDF.withColumnRenamed("frame.number", "frame_number2").withColumnRenamed("_ws.col.Protocol", "protocol2")
 
-// 对同一个 DataFrame 执行自连接（self-join）以查找具有相同源 IP 和目标 IP 但协议不同的记录
 val joinedDF = df1Renamed.alias("df1").join(df2Renamed.alias("df2"),
   $"df1.`ip.src`" === $"df2.`ip.src`" &&
   $"df1.`ip.dst`" === $"df2.`ip.dst`" &&
   $"df1.protocol1" =!= $"df2.protocol2" &&
-  $"df1.frame_number1" =!= $"df2.frame_number2"  // 确保连接的是不同的帧
+  $"df1.frame_number1" =!= $"df2.frame_number2" // 确保不是相同的记录
 )
 
 // 显示结果
@@ -48,6 +49,7 @@ joinedDF.select(
   $"df1.protocol1", 
   $"df2.protocol2"
 ).show()
+
 
 
 joinedDF.count()
