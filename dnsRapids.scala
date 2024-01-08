05572eed4cd62151114d814a4fe35286a2752fd9
mydoors <zhengjieormydoors@gmail.com>
2024-01-08 18:47:36 +0800

    add

--- dnsRapids.scala
+++ dnsRapids.scala
@@ -27,25 +27,20 @@ val preprocessedDf = Preprocess(rawDf)
 // GetDnsInfos函数
 def GetDnsInfos(df: DataFrame): DataFrame = {
   val filteredDf = df.filter($"silkAppLabel" === 53 && $"sourceIpAddress".startsWith("192.168.1"))
-    .select((networkFields ++ Array("dnsRecordList")).map(col): _*)
-
-  // 展开dnsRecords并选择query和response的子字段
-  val expandedDf = filteredDf
     .select(
-      $"network.frame.number".as("number"),
-      $"network.frame.protocols".as("protocols"),
-      $"network.ethernet.source_mac".as("source_mac"),
-      $"network.ethernet.destination_mac".as("destination_mac"),
-      $"network.ip.source_ip".as("source_ip"),
-      $"network.ip.destination_ip".as("destination_ip"),
-      $"network.udp.source_port".as("source_port"),
-      $"network.udp.destination_port".as("destination_port"),
+      $"number",
+      $"protocols",
+      $"source_mac",
+      $"destination_mac",
+      $"source_ip",
+      $"destination_ip",
+      $"source_port",
+      $"destination_port",
       explode($"dnsRecordList.query.queries").as("query"),
       $"dnsRecordList.response.*"
     )
 
-  // 从展开的记录中提取信息
-  val retdf = expandedDf
+  val retdf = filteredDf
     .withColumn("id", $"query.transaction_id")
     .withColumn("name", $"query.name")
     .withColumn("ttl", $"query.ttl")
@@ -63,9 +58,34 @@ def GetDnsInfos(df: DataFrame): DataFrame = {
     .withColumn("dnsSRV", $"response.dnsSRV")
     .withColumn("dnsPTR", $"response.dnsPTR")
 
-  // 选择相关列
-  retdf.select((networkFields ++ Array("id", "name", "ttl", "QR", "type", "rCode", "section", "dnsA", "dnsAAAA", "dnsCNAME", "dnsMX", "dnsNS", "dnsTXT", "dnsSOA", "dnsSRV", "dnsPTR")).map(col): _*)
+  retdf.select(
+    $"number",
+    $"protocols",
+    $"source_mac",
+    $"destination_mac",
+    $"source_ip",
+    $"destination_ip",
+    $"source_port",
+    $"destination_port",
+    $"id",
+    $"name",
+    $"ttl",
+    $"type",
+    $"QR",
+    $"rCode",
+    $"section",
+    $"dnsA",
+    $"dnsAAAA",
+    $"dnsCNAME",
+    $"dnsMX",
+    $"dnsNS",
+    $"dnsTXT",
+    $"dnsSOA",
+    $"dnsSRV",
+    $"dnsPTR"
+  )
 }
+
 
 // 应用GetDnsInfos函数处理数据
 val dnsInfosDf = GetDnsInfos(preprocessedDf)
